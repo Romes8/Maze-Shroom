61edf7ed1fc65d3fa36441bb0ee0a659dba54be7
Gustav Samuel Bonde Holmgren <guho@itu.dk>
2023-09-29 15:09:18 +0200

    player movement
    added a player with placeholder image, movement and control to the player and made it so that the camera follows the player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speedmod = 0.03f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal")*speedmod, Input.GetAxis("Vertical")*speedmod, 0));
+    }
+}

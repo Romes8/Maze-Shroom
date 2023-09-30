4435a6b0f25e215283a5a9bb4d033f053b0bc093
Roman <58208649+Romes8@users.noreply.github.com>
2023-09-30 18:14:09 +0200

    Game Logic
    Added:
    - Detection of mushroom hit - mushroom gets deleted
    - collection of mushroom names (colour names)
    - generating potion at the start of the game
    - comparison of generated array and collected array - if equal win condition is met

---
+++ Assets/Logic.cs
@@ -0,0 +1,115 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class Logic : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    object mushroom_colour;
+
+    string selected_recipe_name = "";
+    List<string> selected_recipe_content = new List<string>();
+    List<string> picked_content = new List<string>();
+
+
+    void Start()
+    {
+        //pick any of the recipes and save it to the array.
+
+        //get random number
+
+        // List of string arrays
+        Dictionary<string, string[]> Recipes = new Dictionary<string, string[]>();
+
+
+        // Create and populate string arrays
+        string[] RedPotion = { "red", "red", "blue" };
+        string[] BrokenHandPotion = { "red", "blue" };
+
+        // Add string arrays to the list
+        Recipes.Add("RedPotion", RedPotion);
+        Recipes.Add("BrokenHandPotion", BrokenHandPotion);
+
+        // Randomly select an array from the list
+        System.Random random = new System.Random();
+        int randomIndex = random.Next(0, Recipes.Count);
+
+
+        selected_recipe_name = Recipes.Keys.ToArray()[randomIndex];
+        string[] selectedrecipe = Recipes[selected_recipe_name];
+
+        // Convert the string array to a list of strings
+        selected_recipe_content = new List<string>(selectedrecipe);
+
+        print("Picked potion is:");
+        print(selected_recipe_name);
+
+        print("Recipe materials: ");
+
+        for (int i = 0; i < selected_recipe_content.Count; i++)
+        {
+            print(selected_recipe_content[i]);
+        }
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("mushroom")) // Assuming you have a "Mushroom" tag for mushrooms
+        {
+            // Handle collision with mushroom here (e.g., destroy the mushroom)
+            print("Colided with mushroom");
+
+            //get the colour of the mushroom
+            mushroom_colour = Variables.Object(collision.gameObject).Get("mushroom-colour");
+
+            //add to inventory
+            picked_content.Add(mushroom_colour.ToString());
+
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        //when player is in the house compare the arrays. if correct than win the game. 
+
+        if (other.CompareTag("home"))
+        {
+            print("Player is at home");
+            print("Picked content. ");
+            print(picked_content.Count);
+
+            print("Selected. ");
+            print(selected_recipe_content.Count);
+
+
+            bool areEqual = AreListsEqualIgnoringOrder(picked_content, selected_recipe_content);
+
+            if (areEqual)
+            {
+                //PLAYER WON THE GAME
+                print("YOU MADE YOUR FIRST POTION NAMED:");
+                print(selected_recipe_name.ToUpper());
+
+                //redirect to the end screen
+            }
+
+
+        }
+    }
+
+    static bool AreListsEqualIgnoringOrder<T>(List<T> list1, List<T> list2)
+    {
+        // Check if the lists have the same elements, regardless of order
+        return list1.Count == list2.Count && list1.All(list2.Contains);
+    }
+}
